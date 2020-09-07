5aa3065a86393544bd445be03d9a7481e420e8e1
Rene Dolores <renecms@gmail.com>
2020-09-06 22:37:46 -0300

    created methods for two different endpoints to extract dividends and bonus data from B3 website for Itau stocks

---
+++ src/index.js
@@ -0,0 +1,88 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+ async function getLatestEvents() {
+    try {
+        const bonusList = []
+        const dividendList = []
+        const { data } = await axios.get(
+            'http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/ResumoEventosCorporativos.aspx?codigoCvm=19348&tab=3&idioma=pt-br'
+        );
+        const $ = cheerio.load(data);
+
+
+        $('#ctl00_contentPlaceHolderConteudo_grdBonificacao_ctl01 > tbody > tr').each((_idx, row) => {
+            $columns = $(row).find('td');
+            currentBonus = {}
+            currentBonus.type = $columns.eq(0).text().trim()
+            currentBonus.isin = $columns.eq(1).text().trim()
+            currentBonus.newsDate = $columns.eq(2).text().trim()
+            currentBonus.comDate = $columns.eq(3).text().trim()
+            currentBonus.percentage = $columns.eq(4).text().trim()
+            currentBonus.bonusIsin = $columns.eq(5).text().trim()
+            currentBonus.observation = $columns.eq(6).text().trim()
+            bonusList.push(currentBonus);
+        });
+
+        $('#ctl00_contentPlaceHolderConteudo_grdDividendo_ctl01 > tbody > tr').each((_idx, row) => {
+            $columns = $(row).find('td');
+            const currentDividend = {}
+            currentDividend.type = $columns.eq(0).text().trim()
+            currentDividend.isin = $columns.eq(1).text().trim()
+            currentDividend.newsDate = $columns.eq(2).text().trim()
+            currentDividend.comDate = $columns.eq(3).text().trim()
+            currentDividend.value = $columns.eq(4).text().trim()
+            currentDividend.relativeTo = $columns.eq(5).text().trim()
+            currentDividend.paymentDate = $columns.eq(6).text().trim()
+            currentDividend.observation = $columns.eq(7).text().trim()
+            dividendList.push(currentDividend);
+        });
+
+        return { bonus: bonusList, dividends: dividendList }
+    } catch (error) {
+        throw error;
+    }
+};
+
+async function getAllDividends() {
+    try {
+        const dividendList = []
+        const { data } = await axios.get(
+            'http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/ResumoProventosDinheiro.aspx?codigoCvm=19348&tab=3.1&idioma=pt-br'
+        );
+        const $ = cheerio.load(data);
+
+        $('#ctl00_contentPlaceHolderConteudo_grdProventos_ctl01 > tbody > tr').each((_idx, row) => {
+            $columns = $(row).find('td');
+            const currentDividend = {}
+            currentDividend.stockType = $columns.eq(0).text().trim()
+            currentDividend.approvalDate = $columns.eq(1).text().trim()
+            currentDividend.amount = $columns.eq(2).text().trim()
+            currentDividend.amountPer = $columns.eq(3).text().trim() // unit or thousand
+            currentDividend.dividendType = $columns.eq(4).text().trim()
+            currentDividend.comDate = $columns.eq(5).text().trim()
+            currentDividend.lastPriceDate = $columns.eq(6).text().trim()
+            currentDividend.comDatePrice = $columns.eq(7).text().trim()
+            currentDividend.comDatePricePer = $columns.eq(8).text().trim() // unit or thousand
+            currentDividend.dividendByPrice = $columns.eq(9).text().trim()
+            dividendList.push(currentDividend);
+        });
+
+        return dividendList
+    } catch (error) {
+        throw error;
+    }
+};
+
+getAllDividends()
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => console.log(err))
+
+// getLatestEvents()
+//     .then((data) => {
+//         console.log(data.bonus);
+//         console.log(data.dividends);
+//     })
+//     .catch((err) => console.log(err))
