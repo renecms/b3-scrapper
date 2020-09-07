2ed7118d587717149c12e3243615edc914e8d7a5
Rene Dolores <renecms@gmail.com>
2020-09-07 00:17:37 -0300

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
